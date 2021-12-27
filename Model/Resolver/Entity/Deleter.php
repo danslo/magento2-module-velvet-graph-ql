47d0cbe0d4007fa38a9808f78ed4b6d9db8ab1a8
Daniel Sloof <goapsychadelic@gmail.com>
2021-12-27 21:48:40 +0100

    set secure area before deleting

--- Model/Resolver/Entity/Deleter.php
+++ Model/Resolver/Entity/Deleter.php
@@ -12,21 +12,25 @@ use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
 
 class Deleter implements ResolverInterface, AdminAuthorizationInterface
 {
     private $entityFactory;
     private AbstractResource $resourceModel;
     private string $aclResource;
+    private Registry $registry;
 
     public function __construct(
         FactoryWrapper $factoryWrapper,
         AbstractResource $resourceModel,
+        Registry $registry,
         string $aclResource
     ) {
         $this->entityFactory = $factoryWrapper->getFactory();
         $this->resourceModel = $resourceModel;
         $this->aclResource = $aclResource;
+        $this->registry = $registry;
     }
 
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
@@ -44,6 +48,7 @@ class Deleter implements ResolverInterface, AdminAuthorizationInterface
         }
 
         try {
+            $this->registry->register('isSecureArea', true);
             $this->resourceModel->delete($entity);
             return true;
         } catch (\Exception $e) {
